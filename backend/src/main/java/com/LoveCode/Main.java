56b0d9322d221ceb08958898ec50e84e6fa1c9a6
Pablo Pedrón <39277526+ppedron1@users.noreply.github.com>
2026-04-17 08:54:06 +0200

    Crear conexion db, prueba sql
    se crea conexión DB se actualiza pom de java 17 a 21, se actualizan dependencias, se prueba sentencia basica sql

--- backend/src/main/java/com/LoveCode/Main.java
+++ backend/src/main/java/com/LoveCode/Main.java
@@ -1,7 +1,41 @@
-package com.example;
+package com.LoveCode;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Iniciando mi aplicación LoveCode...\n");
+        
+        Connection conn = ConexionDB.conectar();
+        
+        if (conn != null) {
+            try {
+    
+                Statement stmt = conn.createStatement();
+                String sql = "SELECT id_usuario, nombre, email FROM Usuarios";
+                
+                ResultSet rs = stmt.executeQuery(sql);
+                
+                System.out.println("\n--- 📝 LISTA DE USUARIOS EN LA BASE DE DATOS ---");
+                
+                while (rs.next()) {
+                    int id = rs.getInt("id_usuario");
+                    String nombre = rs.getString("nombre");
+                    String email = rs.getString("email");
+                    
+                    System.out.println("ID: " + id + " | Nombre: " + nombre + " | Email: " + email);
+                }
+                
+                System.out.println("------------------------------------------------\n");
+                
+                rs.close();
+                stmt.close();
+                
+            } catch (Exception e) {
+                System.out.println("Error al hacer la consulta: " + e.getMessage());
+            }
+        }
     }
 }

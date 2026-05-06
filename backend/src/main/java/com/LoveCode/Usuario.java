81d7404a208945177497571982205ae73be61706
Pablo Pedrón <39277526+ppedron1@users.noreply.github.com>
2026-05-06 09:28:48 +0200

    Se crea Auth para login y register

---
+++ backend/src/main/java/com/LoveCode/Usuario.java
@@ -0,0 +1,22 @@
+package com.LoveCode;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String email;
+    private String password;
+    private String ciudad;
+
+    // Constructores
+    public Usuario() {}
+    
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getCiudad() { return ciudad; }
+    public void setCiudad(String ciudad) { this.ciudad = ciudad; }
+}
